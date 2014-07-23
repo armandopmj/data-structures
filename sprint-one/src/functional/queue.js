e3f12714ad4e4d2cf2d690e13ef9080ca8ea9d07
Mike Luby <Michael@mLuby.com>
2014-07-23 14:58:27 -0700

    Passed functional stack and queue.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var makeQueue = function(){
   var someInstance = {};
+  var first_i = last_i = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,20 @@ var makeQueue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    storage[ last_i ] = value;
+    last_i++;
   };
 
   someInstance.dequeue = function(){
+    var value = storage[first_i];
+    if( first_i < last_i ){
+      first_i++;
+    }
+    return value;
   };
 
   someInstance.size = function(){
+    return last_i - first_i;
   };
 
   return someInstance;
