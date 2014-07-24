73d7d62140527e2676a6be1a2c49466e9e7ae0cc
Mike Luby <Michael@mLuby.com>
2014-07-24 10:51:45 -0700

    Passed functional-shared & pseudo-classical queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,32 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var some_instance = {
+    storage: {},
+    first: 0,
+    last: 0
+  };
+  _.extend(some_instance, queueMethods);
+  return some_instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[ this.last ] = value;
+    this.last++;
+  },
+  dequeue: function(){
+    var value = this.storage[ this.first ];
+    if( this.size() > 0 ){
+      this.first++;
+    }
+    return value;
+  },
+  size: function(){
+    return this.last - this.first;
+  }
+
+};
 
 
 
