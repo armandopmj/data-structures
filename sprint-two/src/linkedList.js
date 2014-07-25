690a85316f82e4b1629c73701beddda340d6c949
Hack Reactor Students <accounts@hackreactor.com>
2014-07-24 22:15:03 -0700

    Completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,48 +4,37 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    // Create new node with value.
     var newNode = makeNode( value );
-
-    // If no nodes, point head to this node.
     if( list.head === null && list.tail === null ){
       list.head = newNode;
       list.tail = newNode;
     }
-
-    // Point previous node's pointer to new node.
-    list.tail.next = newNode; //
-
-    // Point tail to new node.
+    list.tail.next = newNode;
     list.tail = newNode;
   };
 
   list.removeHead = function(){
     var value = list.head.value;
-    // removing element at head
-    // switch head
+    list.head = list.head.next;
     return value;
   };
 
   list.contains = function(target){
+    var node = arguments.length === 2 ? arguments[1] : list.head;
+    if( node.value === target ) return true;
+    if( node.next === null ) return false;
+    return list.contains( target, node.next )
   };
-
   return list;
 };
 
 var makeNode = function(value){
   var node = {};
-
   node.value = value;
   node.next = null;
-
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-// var linkedList = makeLinkedList();
-// linkedList.addToTail( 12 );
-// return linkedList.head; // { value:12, next: null }
