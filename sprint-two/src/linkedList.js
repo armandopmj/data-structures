b4a68136956dcacab46db7e7415a40c4571fc292
Hack Reactor Students <accounts@hackreactor.com>
2014-07-24 20:01:20 -0700

    Passed LinkedList addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,27 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // Create new node with value.
+    var newNode = makeNode( value );
+
+    // If no nodes, point head to this node.
+    if( list.head === null && list.tail === null ){
+      list.head = newNode;
+      list.tail = newNode;
+    }
+
+    // Point previous node's pointer to new node.
+    list.tail.next = newNode; //
+
+    // Point tail to new node.
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var value = list.head.value;
+    // removing element at head
+    // switch head
+    return value;
   };
 
   list.contains = function(target){
@@ -27,3 +45,7 @@ var makeNode = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var linkedList = makeLinkedList();
+// linkedList.addToTail( 12 );
+// return linkedList.head; // { value:12, next: null }
